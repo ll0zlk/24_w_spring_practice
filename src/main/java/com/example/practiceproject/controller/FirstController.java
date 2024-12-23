91fe0c24f072485abcc8e6f3ae2d11bab9375b41
ll0zlk <hyojin041025@gmail.com>
2024-12-24 00:23:40 +0900

    chap2/모델 추가하기

--- src/main/java/com/example/practiceproject/controller/FirstController.java
+++ src/main/java/com/example/practiceproject/controller/FirstController.java
@@ -1,12 +1,14 @@
 package com.example.practiceproject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FirstController {
     @GetMapping("/hi")      // URL 요청 접수
-    public String niceToMeetYou() {
+    public String niceToMeetYou(Model model) {
+        model.addAttribute("username", "효진");   // 변수 등록 시 addAttribute 메서드 사용
         return "greetings";     // greetings.mustache 파일 반환
     }
 }
