cd7ae0da60a318b20008233c817354622443744a
ll0zlk <hyojin041025@gmail.com>
2024-12-24 17:07:14 +0900

    chap3/DTO를 엔티티로 변경

--- src/main/java/com/example/practiceproject/controller/ArticleController.java
+++ src/main/java/com/example/practiceproject/controller/ArticleController.java
@@ -1,6 +1,7 @@
 package com.example.practiceproject.controller;
 
 import com.example.practiceproject.dto.ArticleForm;
+import com.example.practiceproject.entity.Article;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,9 @@ public class ArticleController {
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form) {
         System.out.println(form.toString());
+        // 1. DTO를 엔티티로 변환
+        Article article = form.toEntity();
+        // 2. 레포지토리로 엔티티를 DB에 저장
         return "";
     }
 }
