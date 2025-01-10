a08dc487f165cd80e8364979f46aae2ed1959d78
ll0zlk <hyojin041025@gmail.com>
2025-01-10 15:45:53 +0900

    chap15/댓글 조회 -> 스트림으로 코드 개선하기

--- src/main/java/com/example/practiceproject/service/CommentService.java
+++ src/main/java/com/example/practiceproject/service/CommentService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CommentService {
@@ -18,7 +19,7 @@ public class CommentService {
     private ArticleRepository articleRepository;
 
     public List<CommentDto> comments(Long articleId) {
-        // 1. 댓글 조회
+        /*// 1. 댓글 조회
         List<Comment> comments = commentRepository.findByArticleId(articleId);
         // 2. 엔티티 -> DTO 변환
         List<CommentDto> dtos = new ArrayList<CommentDto>();
@@ -26,8 +27,11 @@ public class CommentService {
             Comment c = comments.get(i);
             CommentDto dto = CommentDto.createCommentDto(c);
             dtos.add(dto);
-        }
+        }*/
         // 3. 결과 반환
-        return dtos;
+        return commentRepository.findByArticleId(articleId) // 댓글 엔티티 목록 조회
+                .stream()   // 댓글 엔티티 목록을 스트림으로 변환
+                .map(comment -> CommentDto.createCommentDto(comment)) // 엔티티를 DTO로 매핑
+                .collect(Collectors.toList());  // 스트림을 리스트로 변환
     }
 }
