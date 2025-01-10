2458f86033ae94e53f0456e0484d1172c31fc86b
ll0zlk <hyojin041025@gmail.com>
2025-01-10 15:41:37 +0900

    chap15/댓글 조회하기

---
+++ src/main/java/com/example/practiceproject/service/CommentService.java
@@ -0,0 +1,33 @@
+package com.example.practiceproject.service;
+
+import com.example.practiceproject.dto.CommentDto;
+import com.example.practiceproject.entity.Comment;
+import com.example.practiceproject.repository.ArticleRepository;
+import com.example.practiceproject.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CommentService {
+    @Autowired
+    private CommentRepository commentRepository;
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    public List<CommentDto> comments(Long articleId) {
+        // 1. 댓글 조회
+        List<Comment> comments = commentRepository.findByArticleId(articleId);
+        // 2. 엔티티 -> DTO 변환
+        List<CommentDto> dtos = new ArrayList<CommentDto>();
+        for (int i = 0; i < comments.size(); i++) {
+            Comment c = comments.get(i);
+            CommentDto dto = CommentDto.createCommentDto(c);
+            dtos.add(dto);
+        }
+        // 3. 결과 반환
+        return dtos;
+    }
+}
